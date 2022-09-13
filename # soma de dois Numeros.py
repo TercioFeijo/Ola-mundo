31c33301ecf4504eeda0986bc41e407ffc016bb7
TercioFeijo <tercio.feijo@gmail.com>
2022-09-13 19:23:46 -0300

    Create # soma de dois Numeros.py

---
+++ # soma de dois Numeros.py
@@ -0,0 +1,8 @@
+# soma de dois Numeros 
+numero1 = int(input('Primeiro número:\n'))
+numero2 = int(input('Segundo número:\n'))
+
+print('A soma é:',numero1 + numero2,"!")
+print('A soma é: %d!' % (numero1 + numero2))
+print('A soma é: {}!'.format(numero1 + numero2))
+print(f'A soma é: {numero1 + numero2}!')
